a824249d8129ef4db44ef17bddf161455c77d806
Lassya aitipamula <161203075+aitipamulalassya@users.noreply.github.com>
2025-07-27 21:19:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,10 @@ app.use(express.json());
 app.use('/api/v1', rootRouter);
 
 const path = require("path");
-const uploadImagesPath = path.join(__dirname, process.env.UPLOAD_DIR, "uploadedImages");
-app.use("/uploadedImages", express.static(uploadImagesPath));
+onst publicDir = path.join(__dirname, process.env.UPLOAD_DIR || "public");
+
+// 👇 Serve static files from /uploadedImages and /uploadedFiles
+app.use("/uploadedImages", express.static(path.join(publicDir, "uploadedImages")));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
