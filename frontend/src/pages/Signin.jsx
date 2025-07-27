d17527a9cee0edb0ae86bc9f32dbb4c90cb19942
Lassya aitipamula <161203075+aitipamulalassya@users.noreply.github.com>
2025-07-27 17:13:22 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -16,7 +16,7 @@ const Signin = () => {
 
     setLoading(true);
     try {
-      const response = await axios.post('http://localhost:3000/api/v1/user/signin', {
+      const response = await axios.post('https://bytebazaar-backend.onrender.com/api/v1/user/signin', {
         username,
         password,
       });
