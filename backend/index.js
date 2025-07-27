2e32b49650654a83a6a9bc9e97df95116652e191
Lassya aitipamula <161203075+aitipamulalassya@users.noreply.github.com>
2025-07-27 18:51:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
+const express  = require("express");
+const rootRouter = require("./routes/RouteIndex");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1',rootRouter);
-const path = require("path");
 
-const uploadImagesPath = path.join(__dirname, process.env.UPLOAD_DIR, "uploadedImages");
-app.use("/uploadedImages", express.static(uploadImagesPath));
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
