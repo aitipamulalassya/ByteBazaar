71f1504bf362ca2011c64c1619493d5baf1897dd
aitipamulalassya <161203075+aitipamulalassya@users.noreply.github.com>
2025-08-19 20:25:27 +0530

    Initial commit

---
+++ backend/routes/Fileupload.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const My_dirName = "C:\\Users\\user\\Downloads\\ByteBazaar-main\\gumroad\\frontend"
+//Example
+
+const fs = require("fs");
+const { authMiddleware } = require("../middlewares/Authmiddleware");
+
+const uploadDir = path.join(__dirname, "..", "public", "uploadedFiles");
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/upload", authMiddleware,upload.single("file"), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ message: "No file uploaded" });
+        }
+        const { path, filename } = req.file;
+        res.json({
+            message: "File uploaded successfully",
+            filePath: path,
+            fileName: filename,
+        });
+    } catch (error) {
+        console.error("Error processing request:", error);
+        res.status(500).json({
+            error: "An error occurred while processing the request",
+        });
+    }
+});
+
+module.exports = router;
