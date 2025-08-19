8a73176a51b6e52bb109f51ddfea036d238c7878
Lassya aitipamula <161203075+aitipamulalassya@users.noreply.github.com>
2025-08-19 21:02:22 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/lassya")
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URL)
 
 const userSchema = new mongoose.Schema({
     username: {
