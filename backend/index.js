fd1e99706daf29674a6a8b503f401c9af2a3ed9c
Lassya aitipamula <161203075+aitipamulalassya@users.noreply.github.com>
2025-08-19 21:03:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const express  = require("express");
 const rootRouter = require("./routes/RouteIndex");
 const cors = require("cors");
 const app = express();
+
+require("dotenv").config();
 app.use("/uploads", express.static(path.join(__dirname, "public", "uploadedImages")));
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1',rootRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server is running on port 3000");
 });
